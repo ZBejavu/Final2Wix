d792885fbf1c482f15de4281034478e438deab10
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-08-24 10:16:42 +0300

    initial installation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
-import Ticket from './components/Ticket'
+//import Ticket from './components/Ticket'
 import './App.css';
 
 function App() {
