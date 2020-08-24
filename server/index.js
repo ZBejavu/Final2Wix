7cb8c565b44f4c71946f7921d12c4e8e16814869
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-08-24 11:16:20 +0300

    hopefully last eslint based push commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
+
 const port = 8080;
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
