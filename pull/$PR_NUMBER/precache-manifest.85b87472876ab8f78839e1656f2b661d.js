cb3f49ba442a2acd26181808a193c6a6fd805452
ZBejavu <ZBejavu@users.noreply.github.com>
2021-06-13 22:01:04 +0000

    Deploying to gh-pages from @ ZBejavu/Final2Wix@fbd1afcaba5dafdb0b011383b56239f245cf2446 🚀

---
+++ pull/$PR_NUMBER/precache-manifest.85b87472876ab8f78839e1656f2b661d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42c47845585a4fcd2cb",
+    "url": "0.443f073b.iframe.bundle.js"
+  },
+  {
+    "revision": "d1374a340036cba4b99f",
+    "url": "4.3d5979ca.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.3d5979ca.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "82232b2cda281a060ac1",
+    "url": "5.56686d8f.iframe.bundle.js"
+  },
+  {
+    "revision": "9bab992173a65c66e81b",
+    "url": "6.3641fec3.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.3641fec3.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "565e0071caa6f36ce0a9",
+    "url": "7.d56bd185.iframe.bundle.js"
+  },
+  {
+    "revision": "9b79f90f9f7ac5cc07d35928d282a31e",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "5f1dd901a90db2c849ab",
+    "url": "main.a43804f9.iframe.bundle.js"
+  },
+  {
+    "revision": "b46564b750ad8e2da62d",
+    "url": "runtime~main.33f3f720.iframe.bundle.js"
+  },
+  {
+    "revision": "ccf3c427e56f2b135e1e5ddf1cfc459f",
+    "url": "static/media/screenshot.ccf3c427.png"
+  },
+  {
+    "revision": "ae747130320961fb9a6c10f14a4a50ac",
+    "url": "static/media/screenshot1.ae747130.png"
+  },
+  {
+    "revision": "ccdd90c07ae98d14b33da93ff14a5544",
+    "url": "static/media/screenshot2.ccdd90c0.png"
+  },
+  {
+    "revision": "cceeee3975a7b6964eee",
+    "url": "vendors~main.2668fd8e.iframe.bundle.js"
+  },
+  {
+    "revision": "8b4da7f8f1fb7e1f13c9f5ab1cc1518b",
+    "url": "vendors~main.2668fd8e.iframe.bundle.js.LICENSE.txt"
+  }
+]);
